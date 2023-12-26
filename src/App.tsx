d973b659fc63d30a4c509cf2831e30dbcd8a178a
kaihsu12 <kaihsu12@gmail.com>
2023-12-26 13:18:26 +0800

    set up router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 
+const basename = process.env.PUBLIC_URL;
+
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <BrowserRouter basename={basename}>
+        <Routes></Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
