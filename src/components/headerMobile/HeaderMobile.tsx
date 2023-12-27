3a311b85c5dcb0ae84bbf4863d31faff2675771e
kaihsu12 <kaihsu12@gmail.com>
2023-12-27 14:11:39 +0800

    create components

---
+++ src/components/headerMobile/HeaderMobile.tsx
@@ -0,0 +1,18 @@
+// img
+
+// styling
+import './HeaderMobile.scss';
+
+const HeaderMobile: React.FC = () => {
+  return (
+    <>
+      <header>
+        <div>
+          <p>hey</p>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default HeaderMobile;
---
+++ src/pages/mainPage/MainPage.tsx
@@ -0,0 +1,12 @@
+// components
+import HeaderMobile from '../../components/headerMobile/HeaderMobile';
+
+const MainPage: React.FC = () => {
+  return (
+    <div>
+      <HeaderMobile />
+    </div>
+  );
+};
+
+export default MainPage;
