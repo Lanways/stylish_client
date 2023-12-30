1b006d0a0ea28ec68fe870a2bf2994878ab3129d
kaihsu12 <kaihsu12@gmail.com>
2023-12-30 15:11:36 +0800

    define type for props and render to page

--- src/components/itemCard/ItemCard.tsx
+++ src/components/itemCard/ItemCard.tsx
@@ -3,16 +3,31 @@ import Sample from '../../assets/item-sample.png';
 //styling
 import './ItemCard.scss';
 
-const ItemCard: React.FC = () => {
+interface ProductProps {
+  productInfo: {
+    id: number;
+    name: string;
+    price: number;
+    createdAt: string;
+    updatedAt: string;
+    image: string;
+    sizeOptions: string;
+    quantity: number;
+    description: string;
+    additionalImage: string;
+  };
+}
+
+const ItemCard: React.FC<ProductProps> = (props) => {
   return (
-    <div className='itemCard'>
+    <div key={props.productInfo.id} className='itemCard'>
       <div>
-        <img src={Sample} alt='logo' />
+        <img src={props.productInfo.image} alt='logo' />
       </div>
       <div className='item-info'>
-        <h3 className='medium-14 item-title'>灰色針織兩件式套裝</h3>
+        <h3 className='medium-14 item-title'>{props.productInfo.name}</h3>
         <div className='price'>
-          <span className='bold-16'>NT$2,400</span>
+          <span className='bold-16'>{`NT$${props.productInfo.price}`}</span>
           <span className='regular-13 regular-price'>NT$1,680</span>
         </div>
       </div>
