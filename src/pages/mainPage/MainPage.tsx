2fcba31a0d3645354bb99047ca34f6a0b823d3c4
kaihsu12 <kaihsu12@gmail.com>
2023-12-30 15:09:09 +0800

    fetch api and define types

--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -1,11 +1,42 @@
+//hooks
+import { useState, useEffect } from 'react';
 // components
 import HeaderMobile from '../../components/headerMobile/HeaderMobile';
 import HeaderDestop from '../../components/headerDestop/HeaderDestop';
 import ItemCard from '../../components/itemCard/ItemCard';
+// api
+import { getProducts } from '../../api/main';
 // styling
 import './MainPage.scss';
 
 const MainPage: React.FC = () => {
+  const [products, setProducts] = useState<
+    {
+      id: number;
+      name: string;
+      price: number;
+      createdAt: string;
+      updatedAt: string;
+      image: string;
+      sizeOptions: string;
+      quantity: number;
+      description: string;
+      additionalImage: string;
+    }[]
+  >([]);
+
+  useEffect(() => {
+    const getProductsAsync = async () => {
+      try {
+        const products = await getProducts();
+        setProducts(products);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getProductsAsync();
+  }, []);
+
   return (
     <div className='mainPage'>
       <HeaderMobile />
