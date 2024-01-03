6e59844c07c6eae9d54c8eadb67dc9a06cd08f12
kaihsu12 <kaihsu12@gmail.com>
2024-01-03 16:56:08 +0800

    create file for self defined types

---
+++ src/types/type.tsx
@@ -0,0 +1,20 @@
+export type Product = {
+  id: number;
+  name: string;
+  price: string;
+  createdAt: string;
+  updatedAt: string;
+  image: string;
+  sizeOptions: string;
+  quantity: number;
+  description: string;
+  additionalImage: string;
+  categoryId: number;
+};
+
+export type Category = {
+  createdAt: string;
+  id: number;
+  name: string;
+  updatedAt: string;
+}[];
