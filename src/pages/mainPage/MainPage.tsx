bf4e03ea44dbfd7470a450ebf48b99881a7c841e
kaihsu12 <kaihsu12@gmail.com>
2024-01-03 16:57:23 +0800

    define type for imported api data and map them out

--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -7,56 +7,80 @@ import ItemCard from '../../components/itemCard/ItemCard';
 import SideMenu from '../../components/sideMenu/SideMenu';
 // api
 import { getProducts } from '../../api/main';
+import { getCategory } from '../../api/main';
+// types
+import { Product } from '../../types/type';
+import { Category } from '../../types/type';
 // styling
 import './MainPage.scss';
 
 const MainPage: React.FC = () => {
   // states
-  const [products, setProducts] = useState<
-    {
-      id: number;
-      name: string;
-      price: number;
-      createdAt: string;
-      updatedAt: string;
-      image: string;
-      sizeOptions: string;
-      quantity: number;
-      description: string;
-      additionalImage: string;
-    }[]
-  >([]);
-
+  const [products, setProducts] = useState<Product[]>([]);
+  const [category, setCategory] = useState<Category>([]);
   const [IsSideMenuShow, setIsSideMenuShow] = useState(false);
+  const [categoryId, setCategoryId] = useState(1);
 
   const IsSideMenuShowHandler = (show: boolean) => {
     setIsSideMenuShow(show);
+  };
+
+  const categoryIdHandler = (id: number) => {
+    setCategoryId(id);
   };
 
   useEffect(() => {
     const getProductsAsync = async () => {
       try {
         const products = await getProducts();
+        console.log(products);
         setProducts(products);
       } catch (error) {
         console.log(error);
       }
     };
+
+    const getCategoryAsync = async () => {
+      try {
+        const category = await getCategory();
+        setCategory(category);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
     getProductsAsync();
+    getCategoryAsync();
   }, []);
 
   return (
     <div className='mainPage'>
       <HeaderMobile />
-      <HeaderDestop setIsShow={IsSideMenuShowHandler} />
+      <HeaderDestop
+        setIsShow={IsSideMenuShowHandler}
+        setMenuId={categoryIdHandler}
+        categoryAll={category}
+      />
       <div className='title'>
         <h1 className='medium-20 sec-title'>本月新品</h1>
       </div>
       <div className='main-content'>
-        <div className='side-part'>{IsSideMenuShow && <SideMenu />}</div>
+        <div className='side-part'>
+          {IsSideMenuShow && (
+            <SideMenu
+              setMenuId={categoryIdHandler}
+              categoryAll={category}
+              products={products}
+            />
+          )}
+        </div>
         <div className='item-sec'>
           {products?.map?.((product) => {
-            return <ItemCard productInfo={product} />;
+            return (
+              product.categoryId === categoryId && (
+                <ItemCard key={product.id} productInfo={product} />
+              )
+            );
           })}
         </div>
       </div>
